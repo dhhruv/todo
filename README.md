fc7833d2fece1030d287e1f9f925ecd4fc69f70a
dhhruv <180320107529.ce.dhruv@gmail.com>
2021-01-02 11:08:44 +0530

    todo.py Code Updated | Build Passing all Test Cases

--- README.md
+++ README.md
@@ -1,51 +1,27 @@
-<h1 align="center" style="margin-top: -4px !important;">Todo List Client</h2>
-<p align="center">
-  <img src="https://img.shields.io/badge/build-passing-brightgreen">
-  <img src="https://img.shields.io/badge/python-3.8-informational">
-  <img src="https://img.shields.io/badge/maintainer-dhhruv-informational">
-  <img src="https://img.shields.io/badge/os-windows-brightgreen">
-  <img src="https://img.shields.io/badge/contributions-welcome-brightgreen">
-</p>
+# Todo List Script..
 
-This Script/Program is written as a part of CoronaSafe Engineering Fellowship Test Problem by Pupilfirst in the Python Programming Language passing all the test cases as given in the .js file. Proof of all Test Cases passing is as shown below.
+This Script/Program is written as a part of CoronaSafe Engineering Fellowship Test Problem by Pupilfirst in the Python Programming Language passing all the test cases as given in the .js file.
 
-<p align="center">
-  <img src="https://user-images.githubusercontent.com/72680045/102444820-13485180-4050-11eb-8de3-ed3b0a86c13d.PNG">
-</p>
-<br>
-
+**Tested on Windows OS.**
+**Author: Dhruv Panchal**
 
 ## Getting started
 
 1. Install Python: Python is usually installed by default on most modern systems. To check what your currently have, open a terminal and run the following command
-
-	```
-	  python3 --version
-
-	```
-  This should output some information on the installed Python version.
-	You can also install ruby by following these instructions. https://installpython3.com/
+    ```
+    python3 --version
+    ```
+    This should output some information on the installed Python version.
+    You can also install ruby by following these instructions. https://installpython3.com/
 
 2. You are expected to write the code in `todo.py` file.
 
-3. Once you are done with the changes you should be able to execute the todo app by running the following commandfrom the terminal.
+3. Once you are done with the changes you should be able to execute the todo app by running the following command from the terminal.
 
-   **On Windows (Command Prompt):**
+   **On Windows:**
 
    ```
    todo.bat
-   ```
-
-   **On Windows (PowerShell):**
-
-   ```
-   ./todo.bat
-   ```
-
-   **On \*nix:**
-
-   ```
-   ./todo.sh
    ```
 
 ## Run Automated Tests
@@ -61,12 +37,7 @@ This Script/Program is written as a part of CoronaSafe Engineering Fellowship Te
    ```
    > mklink todo todo.bat
    ```
-
-   **On \*nix:**
-
-   ```
-   $ ln -s todo.sh todo
-   ```
+   **Note: This is a mandatory step else you've to use `todo.bat` instead of `todo` in the Command Prompt in Windows OS.**
 
 4. Now run `npm test` and you will see all the tests failing. As you fill in each functionality, you can re-run the tests to see them passing one by one.
 
@@ -76,10 +47,10 @@ This Script/Program is written as a part of CoronaSafe Engineering Fellowship Te
 
 2. The app should read from and write to a `todo.txt` text file. Each todo item occupies a single line in this file. Here is an example file that has 2 todo items.
 
-```txt
-water the plants
-change light bulb
-```
+    ```txt
+    water the plants
+    change light bulb
+    ```
 
 3.  When a todo item is completed, it should be removed from `todo.txt` and instead added to the `done.txt` text file. This file has a different format:
 
@@ -93,14 +64,29 @@ change light bulb
 
     The date when the todo is marked as completed is recorded in the `yyyy-mm-dd` format (ISO 8601). For example, a date like `15th August, 2020` is represented as `2020-08-15`.
 
+    **Note:- Here I added a something out of the box where `todo report` will generate the pending and completed tasks of the current day so if there are completed tasks in `done.txt` of days other than current day then they won't be counted in the report of current day.**
+
 4.  The application must open the files `todo.txt` and `done.txt` from where the app is run, and not where the app is located. For example, if we invoke the app like this:
 
     ```
     $ cd ~/plans
     $ ~/apps/todo ls
     ```
-
     The application should look for the text files in `~/plans`, since that is the user’s current directory.
+
+    **Note:- To make the above specification work, I changed the `todo.bat` from**
+    ```
+    @echo off
+    python3 todo.py %1 %2
+    ``` 
+    **to**
+    ```
+    @echo off
+    python "%~dp0\todo.py" %1 %2
+    ```
+    **This will operate the `todo.py` Script from the Current Directory where the Command Prompt is used so the above mentioned specification is solved.**
+
+
 
 ## Usage
 
@@ -118,6 +104,7 @@ $ ./todo done NUMBER      # Complete a todo
 $ ./todo help             # Show usage
 $ ./todo report           # Statistics
 ```
+**Note: If there are arguments after help for eg. `todo help abc` then it will throw an Exception due to given specifications in the Initial README and will recommend to use `todo help`.**
 
 ### 2. List all pending todos
 
@@ -128,6 +115,7 @@ $ ./todo ls
 [2] change light bulb
 [1] water the plants
 ```
+**Note: If there are arguments after ls for eg. `todo ls abc` then it will throw an Exception due to given specifications in the Initial README and will recommend to use `todo help`.**
 
 ### 3. Add a new todo
 
@@ -153,6 +141,7 @@ Attempting to delete a non-existent todo item should display an error message.
 $ ./todo del 5
 Error: todo #5 does not exist. Nothing deleted.
 ```
+**Note: The Script will throw an Exception if the passed argument is not an integer for eg. `todo del abc` will throw an Exception so it is mandatory to pass an integer.**
 
 ### 5. Mark a todo item as completed
 
@@ -169,6 +158,7 @@ Attempting to mark a non-existed todo item as completed will display an error me
 $ ./todo done 5
 Error: todo #5 does not exist.
 ```
+**Note: The Script will throw an Exception if the passed argument is not an integer for eg. `todo done abc` will throw an Exception so it is mandatory to pass an integer.**
 
 ### 6. Generate a report
 
@@ -178,3 +168,4 @@ Use the `report` command to see the latest tally of pending and completed todos.
 $ ./todo report
 dd/mm/yyyy Pending : 1 Completed : 4
 ```
+**Note: If there are arguments after report for eg. `todo report abc` then it will throw an Exception due to given specifications in the Initial README and will recommend to use `todo help`. Here I added a something out of the box where `todo report` will generate the pending and completed tasks of the current day so if there are completed tasks in `done.txt` of days other than current day then they won't be counted in the report of current day.**
--- todo.py
+++ todo.py
@@ -1,8 +1,5 @@
-"""
-Author: Dhruv Panchal
-This Script/Program is written as a part of CoronaSafe Engineering Fellowship Test Problem
-in the Python Programming Language passing all the test cases as given in the .js file.
-"""
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import sys
 import os.path
@@ -11,148 +8,187 @@ from datetime import datetime
 
 def printHelp():
 
-	# Function to print Usage Section when asked for help or when no argur=ement is passed.
-	todohelp="""Usage :-
+    todohelp = \
+        """Usage :-
 $ ./todo add "todo item"  # Add a new todo
 $ ./todo ls               # Show remaining todos
 $ ./todo del NUMBER       # Delete a todo
 $ ./todo done NUMBER      # Complete a todo
 $ ./todo help             # Show usage
 $ ./todo report           # Statistics"""
-	sys.stdout.buffer.write(todohelp.encode('utf8'))		# Print the Usage in UTF-8 Encoding as the default print() generates unexpected results.
-	
+    sys.stdout.buffer.write(todohelp.encode('utf8'))
+
 
 def addToList(st):
-	
-	# Function to add a New Todo in the todo.txt file.
-	if os.path.isfile('todo.txt'):					# If exists todo.txt then appends the task in the first line.
-	    with open("todo.txt",'r') as todoFileOri:
-	    	data=todoFileOri.read()
-	    with open("todo.txt",'w') as todoFileMod:
-	    	todoFileMod.write(st+'\n'+data)
-	else:											# If not then creates a new file and adds the task.
-	    with open("todo.txt",'w') as todoFile:
-	    	todoFile.write(st+'\n')
-	print('Added todo: "{}"'.format(st))
+
+    if os.path.isfile('todo.txt'):
+        with open('todo.txt', 'r') as todoFileOri:
+            data = todoFileOri.read()
+        with open('todo.txt', 'w') as todoFileMod:
+            todoFileMod.write(st + '\n' + data)
+    else:
+        with open('todo.txt', 'w') as todoFile:
+            todoFile.write(st + '\n')
+    sys.stdout.buffer.write('Added todo: "{}"'.format(st).encode('utf8'
+                            ))
 
 
 def showList():
 
-	# Function to List and print the available todos' in the latest format order.
-	if os.path.isfile('todo.txt'):
-	    with open("todo.txt",'r') as todoFileOri:
-	    	data=todoFileOri.readlines()
-	    ct=len(data)
-	    st=""
-	    for line in data:
-	    	st+='[{}] {}'.format(ct,line)
-	    	ct-=1
-	    sys.stdout.buffer.write(st.encode('utf8'))			# Print the Tasks in Reverse Order in UTF-8 Encoding as the default print() generates unexpected results.
-	else:
-	    print ("There are no pending todos!") 
+    if os.path.isfile('todo.txt'):
+        with open('todo.txt', 'r') as todoFileOri:
+            data = todoFileOri.readlines()
+        ct = len(data)
+        st = ''
+        for line in data:
+            st += '[{}] {}'.format(ct, line)
+            ct -= 1
+        sys.stdout.buffer.write(st.encode('utf8'))
+    else:
+        sys.stdout.buffer.write('There are no pending todos!'.encode('utf8'
+                                ))
 
 
 def delFromList(num):
 
-	# Function to Delete the task from the List. (If available)
-	if os.path.isfile('todo.txt'):
-	    with open("todo.txt",'r') as todoFileOri:
-	    	data=todoFileOri.readlines()
-	    ct=len(data)
-	    if num>ct or num<=0:
-	    	print(f"Error: todo #{num} does not exist. Nothing deleted.")
-	    else:
-	    	with open("todo.txt",'w') as todoFileMod:
-	    		for line in data:
-	    			if ct!=num:
-	    				todoFileMod.write(line)
-	    			ct-=1
-	    	print("Deleted todo #{}".format(num))
-	else:
-	    print(f"Error: todo #{num} does not exist. Nothing deleted.")
+    if os.path.isfile('todo.txt'):
+        with open('todo.txt', 'r') as todoFileOri:
+            data = todoFileOri.readlines()
+        ct = len(data)
+        if num > ct or num <= 0:
+            sys.stdout.buffer.write('Error: todo #{} does not exist. Nothing deleted.'.format(num).encode('utf8'
+                                    ))
+        else:
+            with open('todo.txt', 'w') as todoFileMod:
+                for line in data:
+                    if ct != num:
+                        todoFileMod.write(line)
+                    ct -= 1
+            sys.stdout.buffer.write('Deleted todo #{}'.format(num).encode('utf8'
+                                    ))
+    else:
+
+        sys.stdout.buffer.write('Error: todo #{} does not exist. Nothing deleted.'.format(num).encode('utf8'
+                                ))
 
 
 def markDone(num):
 
-	# Function to mark the given task as Done. (If available)
-	if os.path.isfile('todo.txt'):
-	    with open("todo.txt",'r') as todoFileOri:
-	    	data=todoFileOri.readlines()
-	    ct=len(data)
-	    if num>ct or num<=0:
-	    	print("Error: todo #{} does not exist.".format(num))
-	    else:
-	    	with open("todo.txt",'w') as todoFileMod:
-	    		if os.path.isfile('done.txt'):						# Produces output according to the availability of done.txt file.
-	    			with open("done.txt",'r') as doneFileOri:
-				    	doneData=doneFileOri.read()
-			    	with open("done.txt",'w') as doneFileMod:
-			    		for line in data:
-			    			if ct==num:
-			    				doneFileMod.write("x "+datetime.today().strftime('%Y-%m-%d')+" "+line)
-			    			else:
-			    				todoFileMod.write(line)
-			    			ct-=1
-			    		doneFileMod.write(doneData)
-		    	else:
-		    		with open("done.txt",'w') as doneFileMod:
-			    		for line in data:
-			    			if ct==num:
-			    				doneFileMod.write("x "+datetime.today().strftime('%Y-%m-%d')+" "+line)
-			    			else:
-			    				todoFileMod.write(line)
-			    			ct-=1
-
-	    	print("Marked todo #{} as done.".format(num))
-	else:
-	    print("Error: todo #{} does not exist.".format(num))
+    if os.path.isfile('todo.txt'):
+        with open('todo.txt', 'r') as todoFileOri:
+            data = todoFileOri.readlines()
+        ct = len(data)
+        if num > ct or num <= 0:
+            sys.stdout.buffer.write('Error: todo #{} does not exist.'.format(num).encode('utf8'
+                                    ))
+        else:
+            with open('todo.txt', 'w') as todoFileMod:
+                if os.path.isfile('done.txt'):
+                    with open('done.txt', 'r') as doneFileOri:
+                        doneData = doneFileOri.read()
+                    with open('done.txt', 'w') as doneFileMod:
+                        for line in data:
+                            if ct == num:
+                                doneFileMod.write('x '
+                                        + datetime.today().strftime('%Y-%m-%d'
+                                        ) + ' ' + line)
+                            else:
+                                todoFileMod.write(line)
+                            ct -= 1
+                        doneFileMod.write(doneData)
+                else:
+                    with open('done.txt', 'w') as doneFileMod:
+                        for line in data:
+                            if ct == num:
+                                doneFileMod.write('x '
+                                        + datetime.today().strftime('%Y-%m-%d'
+                                        ) + ' ' + line)
+                            else:
+                                todoFileMod.write(line)
+                            ct -= 1
+            sys.stdout.buffer.write('Marked todo #{} as done.'.format(num).encode('utf8'
+                                    ))
+    else:
+        sys.stdout.buffer.write('Error: todo #{} does not exist.'.format(num).encode('utf8'
+                                ))
 
 
 def generateReport():
 
-	# Function to Generate the Report.
-	countTodo=0
-	countDone=0
-	if os.path.isfile('todo.txt'):
-	    with open("todo.txt",'r') as todoFile:
-	    	todoData=todoFile.readlines()
-	    countTodo=len(todoData)
-	if os.path.isfile('done.txt'):
-	    with open("done.txt",'r') as doneFile:
-	    	doneData=doneFile.readlines()
-	    countDone=len(doneData)
-	st=datetime.today().strftime('%Y-%m-%d') + " Pending : {} Completed : {}".format(countTodo,countDone)
-	sys.stdout.buffer.write(st.encode('utf8'))
+    countTodo = 0
+    countDone = 0
+    if os.path.isfile('todo.txt'):
+        with open('todo.txt', 'r') as todoFile:
+            todoData = todoFile.readlines()
+        countTodo = len(todoData)
+    if os.path.isfile('done.txt'):
+        countDone = 0
+        with open('done.txt', 'r') as doneFile:
+            doneData = doneFile.readlines()
+            for i in doneData:
+                temp = i.split()
+                if temp[1] == str(datetime.today().strftime('%Y-%m-%d'
+                                  )):
+                    countDone += 1
+    st = datetime.today().strftime('%Y-%m-%d') \
+        + ' Pending : {} Completed : {}'.format(countTodo, countDone)
+    sys.stdout.buffer.write(st.encode('utf8'))
 
 
-def main(): 
+def main():
 
-	# Main Function
-	if len(sys.argv)==1:
-		printHelp()
-	elif sys.argv[1]=='help':
-		printHelp()
-	elif sys.argv[1]=='ls':
-		showList()
-	elif sys.argv[1]=='add':
-		if len(sys.argv)>2:
-			addToList(sys.argv[2])
-		else:
-			print("Error: Missing todo string. Nothing added!")
-	elif sys.argv[1]=='del':
-		if len(sys.argv)>2:
-			delFromList(int(sys.argv[2]))
-		else:
-			print("Error: Missing NUMBER for deleting todo.")
-	elif sys.argv[1]=='done':
-		if len(sys.argv)>2:
-			markDone(int(sys.argv[2]))
-		else:
-			print("Error: Missing NUMBER for marking todo as done.")
-	elif sys.argv[1]=='report':
-		generateReport()
-	else:
-		print('Option Not Available. Please use "./todo help" for Usage Information')
+    if len(sys.argv) == 1:
+        printHelp()
+    elif sys.argv[1] == 'help':
+        if len(sys.argv) == 2:
+            printHelp()
+        else:
+            sys.stdout.buffer.write('Too Many Arguments for help! Please use "./todo help" for Usage Information'.encode('utf8'
+                                    ))
+    elif sys.argv[1] == 'ls':
+        if len(sys.argv) == 2:
+            showList()
+        else:
+            sys.stdout.buffer.write('Too Many Arguments for ls! Please use "./todo help" for Usage Information'.encode('utf8'
+                                    ))
+    elif sys.argv[1] == 'add':
+        if len(sys.argv) == 3:
+            addToList(sys.argv[2])
+        else:
+            sys.stdout.buffer.write('Error: Missing todo string. Nothing added!'.encode('utf8'
+                                    ))
+    elif sys.argv[1] == 'del':
+        if len(sys.argv) == 3:
+            try:
+                val = int(sys.argv[2])
+                delFromList(int(sys.argv[2]))
+            except ValueError:
+                sys.stdout.buffer.write('Given arguement for del is not an integer! Please use "./todo help" for Usage Information'.encode('utf8'
+                        ))
+        else:
+            sys.stdout.buffer.write('Error: Missing NUMBER for deleting todo.'.encode('utf8'
+                                    ))
+    elif sys.argv[1] == 'done':
+        if len(sys.argv) == 3:
+            try:
+                val = int(sys.argv[2])
+                markDone(int(sys.argv[2]))
+            except ValueError:
+                sys.stdout.buffer.write('Given arguement for done is not an integer! Please use "./todo help" for Usage Information'.encode('utf8'
+                        ))
+        else:
+            sys.stdout.buffer.write('Error: Missing NUMBER for marking todo as done.'.encode('utf8'
+                                    ))
+    elif sys.argv[1] == 'report':
+        if len(sys.argv) == 2:
+            generateReport()
+        else:
+            sys.stdout.buffer.write('Too Many Arguments for report! Please use "./todo help" for Usage Information'.encode('utf8'
+                                    ))
+    else:
+        sys.stdout.buffer.write('Option Not Available. Please use "./todo help" for Usage Information'.encode('utf8'
+                                ))
 
-if __name__=="__main__": 
-    main() 
+
+if __name__ == '__main__':
+    main()
